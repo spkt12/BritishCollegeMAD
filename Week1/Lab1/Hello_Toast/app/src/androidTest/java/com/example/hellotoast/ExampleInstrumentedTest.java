e7c517682b4c458b87171661586866c60c78bbbb
Shankar Sapkota <spktshankar@gmail.com>
2019-08-25 18:14:27 +0545

    Toast Final

--- Week1/Lab1/Hello_Toast/app/src/androidTest/java/com/example/hellotoast/ExampleInstrumentedTest.java
+++ Week1/Lab1/Hello_Toast/app/src/androidTest/java/com/example/hellotoast/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.hello_toast;
+package com.example.hellotoast;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.hello_toast", appContext.getPackageName());
+        assertEquals("com.example.hellotoast", appContext.getPackageName());
     }
 }
--- Week1/Lab1/Hello_Toast/app/src/test/java/com/example/hellotoast/ExampleUnitTest.java
+++ Week1/Lab1/Hello_Toast/app/src/test/java/com/example/hellotoast/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.hello_toast;
+package com.example.hellotoast;
 
 import org.junit.Test;
 

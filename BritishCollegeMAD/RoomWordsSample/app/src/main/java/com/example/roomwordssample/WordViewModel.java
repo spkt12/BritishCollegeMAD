5b92b56773cc8b46927cd3abfda2f1769f49e787
Shankar Sapkota <spktshankar@gmail.com>
2019-11-13 14:34:26 +0545

    Final 2019 project

---
+++ BritishCollegeMAD/RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordViewModel.java
@@ -0,0 +1,31 @@
+package com.example.roomwordssample;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.roomwordssample.MainActivity;
+import com.example.roomwordssample.WordRepository;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+    // Using LiveData and caching what getAlphabetizedWords returns has several benefits:
+    // - We can put an observer on the data (instead of polling for changes) and only update the
+    //   the UI when the data actually changes.
+    // - Repository is completely separated from the UI through the ViewModel.
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+}
